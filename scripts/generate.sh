fce45ec1e381dee50a3b08fa9ee2cf6630496978
jianzhnie <jianzhnie@126.com>
2023-06-02 09:34:34 +0800

    add scripts

---
+++ scripts/generate.sh
@@ -0,0 +1,15 @@
+python qlora.py \
+    --model_name_or_path EleutherAI/pythia-12b \
+    --output_dir ./output \
+    --do_train False \
+    --do_eval False \
+    --do_predict True \
+    --predict_with_generate \
+    --per_device_eval_batch_size 4 \
+    --dataset alpaca \
+    --source_max_len 512 \
+    --target_max_len 128 \
+    --max_new_tokens 64 \
+    --do_sample \
+    --top_p 0.9 \
+    --num_beams 1 \
