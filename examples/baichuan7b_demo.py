01ee1e333423a9e4b0edf8de87ab76c49a7fab55
jianzhnie <jianzhnie@126.com>
2023-06-16 15:42:40 +0800

    update examples

--- examples/baichuan7b_demo.py
+++ examples/baichuan7b_demo.py
@@ -1,5 +1,5 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 def main(load_in_8bit=True, model_path=''):
