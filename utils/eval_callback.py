95fd2b865430cb16d1d296798ccd5942322f4206
jianzhnie <jianzhnie@126.com>
2023-06-17 12:17:52 +0800

    update MMLUEvalCallback

--- utils/eval_callback.py
+++ utils/eval_callback.py
@@ -13,21 +13,22 @@ from transformers import (PreTrainedModel, PreTrainedTokenizer, Trainer,
 
 class MMLUEvalCallback(TrainerCallback):
     """
-    A callback function called after each evaluation step during training.
+    A callback function called after each evaluation step during training to evaluate the performance of a model on an
+    MMLU (Mean Length of Utterance) dataset.
 
     Args:
         trainer (Trainer): The trainer instance to be used.
-        tokenizer (PreTrainedTokenizer): the tokenizer associated with the model.
+        tokenizer (PreTrainedTokenizer): The tokenizer associated with the model being trained.
+        data_dir (str): The directory where the MMLU dataset is stored.
         args (argparse.Namespace): The command line arguments for the current run.
     """
     def __init__(
         self,
-        trainer: Trainer,
+        trainer: 'Trainer',
         tokenizer: PreTrainedTokenizer,
         data_dir: str,
         args: argparse.Namespace,
     ) -> None:
-
         self.trainer = trainer
         self.tokenizer = tokenizer
 
@@ -40,7 +41,8 @@ class MMLUEvalCallback(TrainerCallback):
                     os.path.join(data_dir, 'mmlu/zero_shot_mmlu_val.json'),
                     'test':
                     os.path.join(data_dir, 'mmlu/zero_shot_mmlu_test.json'),
-                })
+                },
+            )
             mmlu_dataset = mmlu_dataset.remove_columns('subject')
         elif args.mmlu_dataset in ['mmlu', 'mmlu-fs']:
             mmlu_dataset = load_dataset(
@@ -50,15 +52,18 @@ class MMLUEvalCallback(TrainerCallback):
                     os.path.join(data_dir, 'mmlu/five_shot_mmlu_val.json'),
                     'test':
                     os.path.join(data_dir, 'mmlu/five_shot_mmlu_test.json'),
-                })
+                },
+            )
         else:
             raise ValueError(
                 f"Invalid value '{args.mmlu_dataset}' for argument 'mmlu_dataset'."
             )
+        
         # Select the appropriate split of the dataset and limit the number of samples to evaluate.
-        mmlu_dataset = mmlu_dataset[args.mmlu_split]
+        self.mmlu_dataset = mmlu_dataset[args.mmlu_split]
         if args.max_mmlu_samples is not None:
-            mmlu_dataset = mmlu_dataset.select(range(args.max_mmlu_samples))
+            self.mmlu_dataset = self.mmlu_dataset.select(
+                range(args.max_mmlu_samples))
 
         # Define a list of token IDs representing the letters A, B, C, and D.
         self.abcd_idx = [
@@ -70,17 +75,22 @@ class MMLUEvalCallback(TrainerCallback):
         # Load the accuracy metric for evaluating MMLU performance.
         self.accuracy = evaluate.load('accuracy')
 
-    def on_evaluate(self, args: Dict[str, Any], state: Dict[str, Any],
-                    control: Dict[str, Any], model: PreTrainedModel,
-                    **kwargs: Any) -> None:
+    def on_evaluate(
+        self,
+        args: Dict[str, Any],
+        state: Dict[str, Any],
+        control: Dict[str, Any],
+        model: PreTrainedModel,
+        **kwargs: Any,
+    ) -> None:
         """
         Iterate over the batches of the evaluation dataset and make predictions for MMLU.
 
         Args:
-            args (Dict[str, Any]): dictionary containing the evaluation arguments.
-            state (Dict[str, Any]): dictionary containing the current state of the trainer.
-            control (Dict[str, Any]): dictionary containing the evaluation control variables.
-            model (PreTrainedModel): the model being evaluated.
+            args (Dict[str, Any]): Dictionary containing the evaluation arguments.
+            state (Dict[str, Any]): Dictionary containing the current state of the trainer.
+            control (Dict[str, Any]): Dictionary containing the evaluation control variables.
+            model (PreTrainedModel): The model being evaluated.
         """
         # Get the evaluation data loader and set the maximum length of the source sequence.
         data_loader = self.trainer.get_eval_dataloader(self.mmlu_dataset)
@@ -104,14 +114,17 @@ class MMLUEvalCallback(TrainerCallback):
             for i, logit in enumerate(logits):
                 label_non_zero_id = (batch['labels'][i] !=
                                      -100).nonzero()[0][0]
+
                 logit_abcd = logit[label_non_zero_id - 1][self.abcd_idx]
                 preds.append(torch.argmax(logit_abcd).item())
 
-            # Extract the ground truth labels and compute the accuracy by subject.
-            IGNORE_INDEX = -100
-            labels = labels[labels != IGNORE_INDEX].view(-1, 2)[:, 0]
-            refs += [self.abcd_idx.index(label) for label in labels.tolist()]
-            loss_mmlu += loss.item()
+                # Extract the ground truth labels and compute the accuracy by subject.
+                IGNORE_INDEX = -100
+                labels = labels[labels != IGNORE_INDEX].view(-1, 2)[:, 0]
+                refs += [
+                    self.abcd_idx.index(label) for label in labels.tolist()
+                ]
+                loss_mmlu += loss.item()
 
         # Extract results by subject.
         results = {'mmlu_loss': loss_mmlu / len(data_loader)}
@@ -122,7 +135,6 @@ class MMLUEvalCallback(TrainerCallback):
             subjects[s]['refs'].append(r)
 
         # Compute the accuracy score for each subject and log the results.
-
         subject_scores = []
         for subject in subjects:
             subject_score = self.accuracy.compute(
