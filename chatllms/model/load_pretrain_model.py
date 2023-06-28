9b2c6ce0125fe08800e878deb4d17c3b4b37a5da
jianzhnie <jianzhnie@126.com>
2023-06-28 14:41:35 +0800

    update models

--- chatllms/model/load_pretrain_model.py
+++ chatllms/model/load_pretrain_model.py
@@ -85,32 +85,38 @@ def load_model_tokenizer(
     # Set compute and torch dtypes based on hyperparameters.
     compute_dtype = (torch.float16 if args.fp16 else
                      (torch.bfloat16 if args.bf16 else torch.float32))
+
+    # 实际的torch数据类型, args.fp16对应torch.float32, args.bf16对应torch.bfloat16, 否则为torch.float32
     torch_dtype = (torch.float32 if args.fp16 else
                    (torch.bfloat16 if args.bf16 else torch.float32))
 
     logger.info(f'Loading base model {args.model_name_or_path}...')
     # Load and prepare pretrained models (without valuehead).
     model = AutoModelForCausalLM.from_pretrained(
-        args.model_name_or_path,
-        device_map=device_map,
-        max_memory=max_memory,
-        low_cpu_mem_usage=True,
-        load_in_4bit=args.bits == 4,
-        load_in_8bit=args.bits == 8,
+        args.model_name_or_path,  # 模型名称或路径
+        device_map=device_map,  # 设备分配方案，可以使用auto，cpu，gpu，还可以使用字典指定每个设备的编号
+        max_memory=max_memory,  # 显存分配方案，还可以使用cpu和硬盘上的内存映射
+        low_cpu_mem_usage=True,  # 是否使用低内存占用的模式
+        load_in_4bit=args.bits == 4,  # 是否使用4bit加载
+        load_in_8bit=args.bits == 8,  # 是否使用8bit加载
+        # BitsAndBytesConfig设置存储格式和计算格式，以及优化方式
         quantization_config=BitsAndBytesConfig(
             load_in_4bit=args.bits == 4,
             load_in_8bit=args.bits == 8,
-            llm_int8_threshold=6.0,
-            llm_int8_has_fp16_weight=False,
-            bnb_4bit_compute_dtype=compute_dtype,
-            bnb_4bit_use_double_quant=args.double_quant,
+            llm_int8_threshold=6.0,  # int8的门限，这个是做什么的
+            llm_int8_has_fp16_weight=False,  # int8的LLM，是否包含fp16的权重
+            bnb_4bit_compute_dtype=compute_dtype,  # 计算时使用的数据类型
+            bnb_4bit_use_double_quant=args.double_quant,  # 是否进行双重量化
             bnb_4bit_quant_type=args.quant_type  # {'fp4', 'nf4'}
         ),
         torch_dtype=torch_dtype,
         **config_kwargs,
     )
     # Print a message if the GPU supports bfloat16.
+    # 如果计算类型为torch.float16并且args.bits==4，也就是4bit量化模型时，进行如下操作。
     if compute_dtype == torch.float16 and args.bits == 4:
+        # 得到显卡的计算能力的最大值和最小值，分别对应major和minor
+        # 只有major>=8时的GPU才支持bfloat16格式，可以使用参数--bf16来加速训练
         major, minor = torch.cuda.get_device_capability()
         if major >= 8:
             logger.info('=' * 80)
@@ -120,6 +126,7 @@ def load_model_tokenizer(
             logger.info('=' * 80)
 
     # Enable model parallelism.
+    # 设置两个和并行操作相关的参数
     setattr(model, 'model_parallel', True)
     setattr(model, 'is_parallelizable', True)
 
@@ -127,13 +134,25 @@ def load_model_tokenizer(
 
     # Prepare the model for k-bit training if specified.
     if not args.full_finetune:
+        # 如果不是全参数微调，那么使用如下函数将模型的参数再进行处理，方便进行int8数据格式的训练微调
         model = prepare_model_for_kbit_training(
             model, use_gradient_checkpointing=args.gradient_checkpointing)
 
     # Enable gradient checkpointing if specified.
     if args.gradient_checkpointing:
+        # 是否使用梯度检查
         model.gradient_checkpointing_enable()
         model.config.use_cache = False  # turn off when gradient checkpointing is enabled
+
+        if hasattr(model, 'enable_input_require_grads'):
+            model.enable_input_require_grads()
+        else:
+
+            def make_inputs_require_grad(module, input, output):
+                output.requires_grad_(True)
+
+            model.get_input_embeddings().register_forward_hook(
+                make_inputs_require_grad)
 
     if not args.full_finetune and hasattr(model, output_embedding_layer_name):
         output_embedding_layer: torch.nn.Linear = getattr(
@@ -169,12 +188,12 @@ def load_model_tokenizer(
             logger.info('Adding LoRA modules...')
             modules = find_all_linear_names(args, model)
             config = LoraConfig(
-                r=args.lora_r,
-                lora_alpha=args.lora_alpha,
-                target_modules=modules,
-                lora_dropout=args.lora_dropout,
-                bias='none',
-                task_type='CAUSAL_LM',
+                r=args.lora_r,  # lora层A矩阵的列大小和B矩阵的行大小
+                lora_alpha=args.lora_alpha,  # 缩放因子
+                target_modules=modules,  # 需要进行lora网络操作的模块名称列表
+                lora_dropout=args.lora_dropout,  # 是否使用dropout，一种正则化操作
+                bias='none',  # 不对偏差参数进行处理
+                task_type='CAUSAL_LM',  # 模型名称，一种标记
             )
             model = get_peft_model(model, config)
 
