045d3995031719457488a2f9ea4b8381decabd7c
jianzhnie <jianzhnie@126.com>
2023-06-29 17:01:52 +0800

    add multiturn train

--- chatllms/data/conv_dataset.py
+++ chatllms/data/conv_dataset.py
@@ -1,26 +1,32 @@
-from typing import Dict
+from typing import Dict, Sequence
 
 import torch
-import transformers
-from fastchat.conversation import SeparatorStyle
-from fastchat.model.model_adapter import get_conversation_template
+from datasets import load_dataset
 from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer
 
 from chatllms.data.data_utils import IGNORE_INDEX as IGNORE_TOKEN_ID
-
-local_rank = None
-
-
-def rank0_print(*args):
-    if local_rank == 0:
-        print(*args)
+from chatllms.data.utils.conversation import Conversation, SeparatorStyle
 
 
 def preprocess(
-    sources,
-    tokenizer: transformers.PreTrainedTokenizer,
+    sources: Sequence[str],
+    tokenizer: PreTrainedTokenizer,
 ) -> Dict:
-    conv = get_conversation_template('vicuna')
+    """Preprocess the data by tokenizing."""
+
+    conv = Conversation(
+        name='vicuna_v1.1',
+        system=
+        'A chat between a curious user and an artificial intelligence assistant. '
+        "The assistant gives helpful, detailed, and polite answers to the user's questions.",
+        roles=['USER', 'ASSISTANT'],
+        messages=(),
+        offset=0,
+        sep_style=SeparatorStyle.ADD_COLON_TWO,
+        sep=' ',
+        sep2='</s>',
+    )
     roles = {'human': conv.roles[0], 'gpt': conv.roles[1]}
 
     # Apply prompt templates
@@ -42,7 +48,7 @@ def preprocess(
         conversations,
         return_tensors='pt',
         padding='max_length',
-        max_length=tokenizer.model_max_length,
+        max_length=512,
         truncation=True,
     ).input_ids
     targets = input_ids.clone()
@@ -76,12 +82,12 @@ def preprocess(
         if False:
             z = target.clone()
             z = torch.where(z == IGNORE_TOKEN_ID, tokenizer.unk_token_id, z)
-            rank0_print(tokenizer.decode(z))
+            print(tokenizer.decode(z))
 
         if cur_len < tokenizer.model_max_length:
             if cur_len != total_len:
                 target[:] = IGNORE_TOKEN_ID
-                rank0_print(
+                print(
                     f'WARNING: tokenization mismatch: {cur_len} vs. {total_len}.'
                     f' (ignored)')
 
@@ -94,10 +100,10 @@ def preprocess(
 
 class SupervisedDataset(Dataset):
     """Dataset for supervised fine-tuning."""
-    def __init__(self, raw_data, tokenizer: transformers.PreTrainedTokenizer):
+    def __init__(self, raw_data, tokenizer: PreTrainedTokenizer):
         super(SupervisedDataset, self).__init__()
 
-        rank0_print('Formatting inputs...')
+        print('Formatting inputs...')
         sources = [example['conversations'] for example in raw_data]
         data_dict = preprocess(sources, tokenizer)
 
@@ -114,3 +120,54 @@ class SupervisedDataset(Dataset):
             labels=self.labels[i],
             attention_mask=self.attention_mask[i],
         )
+
+
+class LazySupervisedDataset(Dataset):
+    """Dataset for supervised fine-tuning."""
+    def __init__(self, raw_data, tokenizer: PreTrainedTokenizer):
+        super(LazySupervisedDataset, self).__init__()
+        self.tokenizer = tokenizer
+
+        print('Formatting inputs...Skip in lazy mode')
+        self.tokenizer = tokenizer
+        self.raw_data = raw_data
+        self.cached_data_dict = {}
+
+    def __len__(self):
+        return len(self.raw_data)
+
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
+        if i in self.cached_data_dict:
+            return self.cached_data_dict[i]
+
+        ret = preprocess([self.raw_data[i]['conversations']], self.tokenizer)
+        ret = dict(
+            input_ids=ret['input_ids'][0],
+            labels=ret['labels'][0],
+            attention_mask=ret['attention_mask'][0],
+        )
+        self.cached_data_dict[i] = ret
+
+        return ret
+
+
+def make_supervised_data_module(tokenizer: PreTrainedTokenizer,
+                                lazy_preprocess, data_path) -> Dict:
+    """Make dataset and collator for supervised fine-tuning."""
+    dataset_cls = (LazySupervisedDataset
+                   if lazy_preprocess else SupervisedDataset)
+    print('Loading data...')
+    if data_path.endswith('.json') or data_path.endswith('.jsonl'):
+        raw_data = load_dataset('json', data_files=data_path)['train']
+    else:
+        raw_data = load_dataset(data_path)['train']
+
+    # Split train/test
+    raw_data = raw_data.train_test_split(test_size=0.1)
+    train_raw_data = raw_data['train']
+    eval_raw_data = raw_data['test']
+    print(f'#train {len(train_raw_data)}, #eval {len(eval_raw_data)}')
+
+    train_dataset = dataset_cls(train_raw_data, tokenizer=tokenizer)
+    eval_dataset = dataset_cls(eval_raw_data, tokenizer=tokenizer)
+    return dict(train_dataset=train_dataset, eval_dataset=eval_dataset)
---
+++ train_multiturn.py
@@ -0,0 +1,93 @@
+import argparse
+import logging
+import pathlib
+from dataclasses import dataclass, field
+from typing import Optional
+
+import transformers
+from transformers import Trainer
+
+from chatllms.data.conv_dataset import make_supervised_data_module
+from chatllms.utils.model_utils import (add_special_tokens_if_missing,
+                                        safe_save_model_for_hf_trainer)
+from train import load_model_tokenizer
+
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default='facebook/opt-125m')
+    trust_remote_code: Optional[bool] = field(
+        default=False,
+        metadata={
+            'help':
+            'Enable unpickling of arbitrary code in AutoModelForCausalLM#from_pretrained.'
+        })
+    use_auth_token: Optional[bool] = field(
+        default=False,
+        metadata={
+            'help':
+            'Enables using Huggingface auth token from Git Credentials.'
+        })
+
+
+@dataclass
+class DataArguments:
+    data_path: str = field(default=None,
+                           metadata={'help': 'Path to the training data.'})
+    lazy_preprocess: bool = True
+
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    cache_dir: Optional[str] = field(default=None)
+    optim: str = field(default='adamw_torch')
+    max_length: int = field(
+        default=2048,
+        metadata={
+            'help':
+            'Maximum sequence length. Sequences will be right padded (and possibly truncated).'
+        },
+    )
+
+
+def train():
+    parser = transformers.HfArgumentParser(
+        (ModelArguments, DataArguments, TrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    args = argparse.Namespace(**vars(model_args), **vars(data_args),
+                              **vars(training_args))
+    # load model and tokenizer
+    logging.warning('Loading model and tokenizer...')
+    model, tokenizer = load_model_tokenizer(args=args)
+    logging.warning('Successfully loaded model and tokenizer.')
+
+    if 'llama' in args.model_name_or_path or 'baichuan' in args.model_name_or_path:
+        logging.warning(
+            f'Adding special tokens for {args.model_name_or_path}.')
+        add_special_tokens_if_missing(tokenizer, model)
+
+    # Add special tokens if they are missing
+    logging.warning('Creating training dataset and data collator.')
+    data_module = make_supervised_data_module(
+        tokenizer=tokenizer,
+        lazy_preprocess=args.lazy_preprocess,
+        data_path=args.data_path,
+    )
+
+    # Initialize the Trainer object and start training
+    logging.warning('Initializing Trainer object.')
+    trainer = Trainer(model=model,
+                      tokenizer=tokenizer,
+                      args=training_args,
+                      **data_module)
+    if list(pathlib.Path(training_args.output_dir).glob('checkpoint-*')):
+        trainer.train(resume_from_checkpoint=True)
+    else:
+        trainer.train()
+    trainer.save_state()
+    safe_save_model_for_hf_trainer(trainer=trainer,
+                                   output_dir=training_args.output_dir)
+
+
+if __name__ == '__main__':
+    train()
