e04a07c3a548c3052962c1d88e1cbcb1e6ce03f6
jianzhnie <jianzhnie@126.com>
2023-07-05 16:43:14 +0800

    update scripts

--- scripts/qlora_finetune/multiturn_llama_finetune.sh
+++ scripts/qlora_finetune/multiturn_llama_finetune.sh
@@ -1,31 +1,25 @@
 python train_qlora.py \
-    --model_name_or_path facebook/opt-125m \
-    --dataset_name olcc \
-    --data_dir /home/robin/prompt_data/ \
-    --load_from_local \
-    --output_dir ./work_dir/run_test \
+    --model_name_or_path  decapoda-research/llama-7b-hf  \
+    --multiturn_dialogue True \
+    --data_path ~/prompt_data/sharegpt/sharegpt_split.json \
+    --output_dir ./work_dir/sharegpt-llama-7b \
     --num_train_epochs 3 \
-    --max_train_samples 100 \
-    --max_eval_samples 100 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 8 \
     --evaluation_strategy steps \
-    --eval_steps 50 \
+    --eval_steps 200 \
     --save_strategy steps \
     --save_total_limit 5 \
-    --save_steps 100 \
+    --save_steps 500 \
     --logging_strategy steps \
-    --logging_steps 1 \
+    --logging_steps 5 \
     --learning_rate 0.0002 \
     --warmup_ratio 0.03 \
     --weight_decay 0.0 \
     --lr_scheduler_type constant \
     --adam_beta2 0.999 \
     --max_grad_norm 0.3 \
-    --max_new_tokens 32 \
-    --source_max_len 512 \
-    --target_max_len 512 \
     --lora_r 64 \
     --lora_alpha 16 \
     --lora_dropout 0.1 \
@@ -33,10 +27,11 @@ python train_qlora.py \
     --quant_type nf4 \
     --fp16 \
     --bits 4 \
+    --model_max_length 2048 \
     --gradient_checkpointing \
+    --lazy_preprocess True \
     --trust_remote_code \
     --do_train \
     --do_eval \
-    --sample_generate \
     --data_seed 42 \
-    --seed 0
+    --seed 0 \
