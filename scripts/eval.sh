325e8ec558ee9fc9e098375e333e44fa89c2bd90
jianzhnie <jianzhnie@126.com>
2023-07-06 15:58:47 +0800

    update scripts

---
+++ scripts/eval.sh
@@ -0,0 +1,7 @@
+python chatllms/evaluation/evaluate_zh.py \
+    --model_name_or_path ~/checkpoints/baichuan7b \
+    --split test  \
+    --data_path ~/prompt_data/ceval-exam \
+    --output_dir ./work_dir/ceval_output
+
+
