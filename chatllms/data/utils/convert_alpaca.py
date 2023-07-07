fad3a6f625e5df5b589e895a5e1c85954713fd23
jianzhnie <jianzhnie@126.com>
2023-07-07 19:59:33 +0800

    update data_convert

--- chatllms/data/utils/convert_alpaca.py
+++ chatllms/data/utils/convert_alpaca.py
@@ -1,26 +1,42 @@
 """
 Convert alpaca dataset into sharegpt format.
 
-Usage: python3 -m fastchat.data.convert_alpaca --in alpaca_data.json
+Usage: python3 -m chatllms.data.convert_alpaca --in alpaca_data.json
 """
 
 import argparse
 import json
+from typing import Any, Dict, List
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--in-file', type=str)
-    parser.add_argument('--out-file', type=str)
-    args = parser.parse_args()
 
-    content = json.load(open(args.in_file, 'r'))
-    new_content = []
-    for i, c in enumerate(content):
-        if len(c['input'].strip()) > 1:
-            q, a = c['instruction'] + '\nInput:\n' + c['input'], c['output']
+def json_dump(obj, path):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(obj, f, indent=2, ensure_ascii=False)
+
+
+def json_load(in_file):
+    with open(in_file, 'r') as f:
+        json_data = json.load(f)
+    return json_data
+
+
+def valid_keys(keys):
+    return all(k in keys for k in ['instruction', 'input', 'output'])
+
+
+def convert_alpaca_vicuna(raw_data: List[Dict[str, Any]]):
+    collect_data = []
+    for i, content in enumerate(raw_data):
+        if not valid_keys(content.keys()):
+            continue
+
+        if len(content['input'].strip()) > 1:
+            q, a = content['instruction'] + '\nInput:\n' + content[
+                'input'], content['output']
         else:
-            q, a = c['instruction'], c['output']
-        new_content.append({
+            q, a = content['instruction'], content['output']
+
+        collect_data.append({
             'id':
             f'alpaca_{i}',
             'conversations': [
@@ -34,9 +50,21 @@ if __name__ == '__main__':
                 },
             ],
         })
+    print(f'Original: {len(raw_data)}, Converted: {len(collect_data)}')
+    return collect_data
 
-    print(f'#out: {len(new_content)}')
-    json.dump(new_content,
-              open(args.out_file, 'w'),
-              indent=2,
-              ensure_ascii=False)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in-file', type=str)
+    parser.add_argument('--out-file', type=str)
+    args = parser.parse_args()
+
+    raw_data = json_load(args.in_file)
+    new_data = convert_alpaca_vicuna(raw_data)
+
+    json_dump(new_data, args.out_file)
+
+
+if __name__ == '__main__':
+    main()
--- scripts/clean_data.sh
+++ scripts/clean_data.sh
@@ -11,3 +11,17 @@ python split_long_conversation.py \
 python clean_evol_instruct.py \
     --in-file /userhome/jianzhnie/prompt_data/WizardLM/WizardLM_evol_instruct_V2_196k/WizardLM_evol_instruct_V2_143k.json \
     --out-file /userhome/jianzhnie/prompt_data/sharegpt/evol_instruct_clean.json
+
+
+python chatllms/data/utils/convert_alpaca.py \
+    --in-file ./prompt_data/chinese_data/alpaca_data_zh_51k.json \
+    --out-file ./prompt_data/chinese_data/alpaca_vicuna.json
+
+
+python chatllms/data/utils/convert_alpaca.py \
+    --in-file ./prompt_data/InstructionWild/instinwild_ch.json \
+    --out-file ./prompt_data/chinese_data/instinwild_ch_vicuna.json
+
+python chatllms/data/utils/convert_alpaca.py \
+    --in-file ./prompt_data/InstructionWild/instinwild_en.json \
+    --out-file ./prompt_data/chinese_data/instinwild_en_vicuna.json
