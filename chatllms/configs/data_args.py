6e7a459607bed0ac813e2ea09cce7905d8845f1a
jianzhnie <jianzhnie@126.com>
2023-07-21 18:20:22 +0800

    update config

--- chatllms/configs/data_args.py
+++ chatllms/configs/data_args.py
@@ -1,29 +1,38 @@
+import os
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import List, Optional
+
+import yaml
+
+
+@dataclass
+class DatasetAttr(object):
+
+    load_from: str
+    dataset_name: Optional[str] = None
+    dataset_sha1: Optional[str] = None
+    dataset_path: Optional[str] = None
+
+    def __repr__(self) -> str:
+        return self.dataset_name
+
+    def __post_init__(self):
+        self.prompt_column = 'instruction'
+        self.query_column = 'input'
+        self.response_column = 'output'
+        self.history_column = None
 
 
 @dataclass
 class DataArguments:
-    multiturn_dialogue: Optional[str] = field(
-        default=False,
-        metadata={
-            'help': 'The dataset is a multiturn_dialogue dataset or not'
-        })
-    lazy_preprocess: bool = True
-    # 微调数据集是alpaca，那么可以试试中文的效果。Llama、Bloom和OPT，或者MPT等等
+    # 微调数据集是 alpaca
     dataset_name: Optional[str] = field(
-        default=None,
+        default='alpaca',
         metadata={
             'help': 'Which dataset to finetune on. See datamodule for options.'
         })
-    data_path: str = field(
-        default=None,
-        metadata={
-            'help':
-            'where is dataset in local dir. See datamodule for options.'
-        })
     # 数据集的本地路径，如果load_from_local为True，那么就从本地加载数据集
-    data_dir: str = field(
+    dataset_dir: str = field(
         default=None,
         metadata={
             'help':
@@ -35,6 +44,12 @@ class DataArguments:
         metadata={
             'help': 'To load the data from local or  huggingface data hub?'
         })
+    multiturn_dialogue: Optional[str] = field(
+        default=False,
+        metadata={
+            'help': 'The dataset is a multiturn_dialogue dataset or not'
+        })
+    lazy_preprocess: bool = True
     prompt_template: str = field(
         default='instruction',
         metadata={
@@ -79,12 +94,39 @@ class DataArguments:
         },
     )
 
-    def __post_init__(self):
-        if self.dataset_name is None and self.data_path and self.data_dir is None:
-            raise ValueError('Need either a dataset name or a data_path .')
-        else:
-            if self.data_path is not None:
-                extension = self.data_path.split('.')[-1]
-                assert extension in [
-                    'json', 'jsonl'
-                ], '`train_file` should be a json or a jsonl file.'
+    def init_for_training(self):  # support mixing multiple datasets
+        dataset_names = [ds.strip() for ds in self.dataset_name.split(',')]
+        this_dir = os.path.dirname(os.path.abspath(__file__))
+        dataset_info_path = os.path.join(this_dir, '../..', 'data',
+                                         'dataset_info.yaml')
+        with open(dataset_info_path, 'r') as f:
+            dataset_info = yaml.safe_load(f)
+
+        self.dataset_list: List[DatasetAttr] = []
+        for i, name in enumerate(dataset_names):
+            if name not in dataset_info:
+                raise ValueError('Undefined dataset {} in {}'.format(
+                    name, dataset_info_path))
+
+            if 'hf_hub_url' in dataset_info[name]:
+                dataset_attr = DatasetAttr(
+                    'hf_hub', dataset_name=dataset_info[name]['hf_hub_url'])
+            elif 'script_url' in dataset_info[name]:
+                dataset_attr = DatasetAttr(
+                    'script', dataset_name=dataset_info[name]['script_url'])
+            elif 'local_path' in dataset_info[name]:
+                dataset_attr = DatasetAttr(
+                    'dataset_path',
+                    dataset_name=dataset_info[name]['local_path'])
+
+            if 'columns' in dataset_info[name]:
+                dataset_attr.prompt_column = dataset_info[name]['columns'].get(
+                    'prompt', None)
+                dataset_attr.query_column = dataset_info[name]['columns'].get(
+                    'query', None)
+                dataset_attr.response_column = dataset_info[name][
+                    'columns'].get('response', None)
+                dataset_attr.history_column = dataset_info[name][
+                    'columns'].get('history', None)
+
+            self.dataset_list.append(dataset_attr)
