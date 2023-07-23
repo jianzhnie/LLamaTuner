df228fbd49435f3c759cbd1bf9d5e3144c93a475
jianzhnie <jianzhnie@126.com>
2023-07-23 18:32:19 +0800

    update dataset_info

--- data/dataset_info.py
+++ data/dataset_info.py
@@ -2,6 +2,16 @@ from os.path import join
 
 
 def get_dataset_info(dataset_dir):
+    """
+    Returns the datasets info to a dataset based on a pre-defined map of dataset names to their corresponding URLs on the internet
+    or local file paths.
+
+    Args:
+        dataset_dir (str): The local directory where the dataset is stored; this is used for datasets that are stored locally.
+
+    Returns:
+        str: The dataset dict to the specified dataset.
+    """
     dataset_info = {
         'alpaca': {
             'hf_hub_url': 'tatsu-lab/alpaca',
