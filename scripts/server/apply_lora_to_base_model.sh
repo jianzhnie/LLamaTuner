bc1e90082e0b8dc2c636572690a3a7280cd95a05
jianzhnie <jianzhnie@126.com>
2023-07-31 10:13:13 +0800

    update scripts

--- scripts/server/apply_lora_to_base_model.sh
+++ scripts/server/apply_lora_to_base_model.sh
@@ -1,4 +1,4 @@
-python chatllms/utils/apply_lora.py \
+CUDA_VISIBLE_DEVICES=0 python chatllms/utils/apply_lora.py \
         --base-model-path ~/checkpoints/baichuan7b/ \
-        --lora-model-path ./work_dir/vicuna_merge_vicuna-baichuan-7b-1gpu/checkpoint-12000 \
-        --target-model-path ./work_dir/vicuna_merge_vicuna-baichuan-7b-1gpu
+        --lora-model-path ./work_dir/vicuna_merge_vicuna-baichuan-7b-1gpu/checkpoint-15000 \
+        --target-model-path ./work_dir/vicuna_merge_vicuna-baichuan-7b-1gpu/merged_model
--- scripts/server/run_inference.sh
+++ scripts/server/run_inference.sh
@@ -1,37 +1,3 @@
-# vicuna_merge
-CUDA_VISIBLE_DEVICES=4 python cli_demo.py \
-    --model_name_or_path ~/checkpoints/baichuan7b/ \
-    --checkpoint_dir ./work_dir/vicuna_merge_vicuna-baichuan-7b-1gpu/checkpoint-12000 \
-    --prompt_template vicuna \
-    --trust_remote_code \
-    --double_quant \
-    --quant_type nf4 \
-    --fp16 \
-    --bits 4
-
-CUDA_VISIBLE_DEVICES=3 python cli_demo.py \
-    --model_name_or_path ~/checkpoints/baichuan7b/ \
-    --checkpoint_dir ./work_dir/vicuna_merge-llama-7b-1gpu/checkpoint-3200 \
-    --prompt_template vicuna \
-    --trust_remote_code \
-    --double_quant \
-    --quant_type nf4 \
-    --fp16 \
-    --bits 4
-
-
 # generated_chat_vicuna
-CUDA_VISIBLE_DEVICES=3 python cli_demo.py \
-    --model_name_or_path ~/checkpoints/baichuan7b/ \
-    --checkpoint_dir ./work_dir/generated_chat_vicuna-baichuan-7b-1gpu/checkpoint-10500 \
-    --prompt_template vicuna \
-    --trust_remote_code \
-    --double_quant \
-    --quant_type nf4 \
-    --fp16 \
-    --bits 4
-
-
-# generated_chat_vicuna
-CUDA_VISIBLE_DEVICES=4 python server/single_chat.py \
-    --model_name_or_path ./work_dir/vicuna_merge_vicuna-baichuan-7b-1gpu/
+CUDA_VISIBLE_DEVICES=0 python single_chat.py \
+    --model_name_or_path ./work_dir/vicuna_merge_vicuna-baichuan-7b-1gpu/merged_model
