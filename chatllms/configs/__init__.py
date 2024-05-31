fb643b89f6f5a7d3120e369a504a85d6ed5aedd3
jianzhnie <jianzhnie@126.com>
2024-05-31 13:35:17 +0800

    update chatllms configs

--- chatllms/configs/__init__.py
+++ chatllms/configs/__init__.py
@@ -1,15 +1,15 @@
-from .data_args import DataArguments
-from .gen_args import GenerationArguments
-from .lora_args import LoraArguments
-from .model_args import ModelArguments
-from .quant_args import QuantArguments
-from .train_args import TrainingArguments
+from chatllms.configs.data_args import DataArguments
+from chatllms.configs.eval_args import EvaluationArguments
+from chatllms.configs.finetune_args import FinetuningArguments
+from chatllms.configs.gen_args import GeneratingArguments
+from chatllms.configs.model_args import ModelArguments
+from chatllms.configs.quant_args import QuantArguments
 
 __all__ = [
     'DataArguments',
-    'GenerationArguments',
+    'GeneratingArguments',
     'ModelArguments',
-    'TrainingArguments',
-    'LoraArguments',
     'QuantArguments',
+    'FinetuningArguments',
+    'EvaluationArguments',
 ]
