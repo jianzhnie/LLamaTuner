a254a03069328dbd846598a0323055deb237709c
jianzhnie <jianzhnie@126.com>
2024-06-05 12:36:43 +0800

    update scripts

--- scripts/eval.sh
+++
@@ -1,5 +0,0 @@
-python llamatuner/evaluation/evaluate_zh.py \
-    --model_name_or_path ~/checkpoints/baichuan7b \
-    --split test  \
-    --data_path ~/prompt_data/ceval-exam \
-    --output_dir ./work_dir/ceval_output
--- scripts/server/apply_lora_to_base_model.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python llamatuner/utils/apply_lora.py \
-        --base-model-path ~/checkpoints/baichuan7b/ \
-        --lora-model-path ./work_dir/vicuna_merge_vicuna-baichuan-7b-1gpu/checkpoint-15000 \
-        --target-model-path ./work_dir/vicuna_merge_vicuna-baichuan-7b-1gpu/merged_model
