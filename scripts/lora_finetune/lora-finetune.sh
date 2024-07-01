dc7f03a6c2b731d6913f7919bf82186a4f581435
jianzhnie <jianzhnie@126.com>
2024-07-01 16:44:09 +0800

    Update scripts

--- scripts/lora_finetune/lora-finetune.sh
+++ scripts/lora_finetune/lora-finetune.sh
@@ -1,19 +1,17 @@
 python tools/train_lora.py \
     --model_name_or_path facebook/opt-125m \
-    --dataset_cfg ./data/run_test.yaml \
+    --dataset alpaca \
     --output_dir work_dir/lora-finetune \
     --wandb_project llamatuner \
     --wandb_run_name alpaca_opt-125m_lora-finetune \
     --num_train_epochs 3 \
-    --max_train_samples 200 \
-    --max_eval_samples 200 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 8 \
-    --evaluation_strategy "steps" \
+    --eval_strategy "steps" \
     --save_strategy "steps" \
-    --save_steps 1 \
-    --save_steps 1 \
+    --eval_steps 100 \
+    --save_steps 500 \
     --save_total_limit 5 \
     --logging_steps 1 \
     --learning_rate 1e-4 \
@@ -23,6 +21,6 @@ python tools/train_lora.py \
     --lr_scheduler_type "cosine" \
     --gradient_checkpointing True \
     --trust_remote_code \
-    --model_max_length 1024 \
+    --model_max_length 128 \
     --do_train \
     --do_eval
