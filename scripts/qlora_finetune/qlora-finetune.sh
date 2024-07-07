3ab2d1c3cf7bf314fe3c8252178b679c50a52ad0
jianzhnie <jianzhnie@126.com>
2024-07-07 10:37:32 +0800

    Update llamtuner scripts

---
+++ scripts/qlora_finetune/qlora-finetune.sh
@@ -0,0 +1,27 @@
+python tools/train_lora.py \
+    --model_name_or_path facebook/opt-125m \
+    --dataset alpaca \
+    --use_qlora \
+    --output_dir work_dir/lora-finetune \
+    --wandb_project llamatuner \
+    --wandb_run_name alpaca_opt-125m_lora-finetune \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --eval_strategy "steps" \
+    --save_strategy "steps" \
+    --eval_steps 100 \
+    --save_steps 500 \
+    --save_total_limit 5 \
+    --logging_steps 1 \
+    --learning_rate 1e-4 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --optim "adamw_torch" \
+    --lr_scheduler_type "cosine" \
+    --gradient_checkpointing True \
+    --trust_remote_code \
+    --model_max_length 128 \
+    --do_train \
+    --do_eval
