eeac277aebe514998fdfa642ea99c8cc1dc2a717
JianZhengNie <jianzhnie@126.com>
2025-01-15 16:24:50 +0800

    Update LLamaTuner

--- llamatuner/train/rm/trainer.py
+++ llamatuner/train/rm/trainer.py
@@ -27,7 +27,8 @@ class PairwiseTrainer(Trainer):
         model: PreTrainedModel,
         inputs: Dict[str, torch.Tensor],
         return_outputs: bool = False,
-    ) -> Union[torch.Tensor, Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]]:
+    ) -> Union[torch.Tensor, Tuple[torch.Tensor, Tuple[torch.Tensor,
+                                                       torch.Tensor]]]:
         r"""
         Computes pairwise loss. The first n examples are chosen and the last n examples are rejected.
 
@@ -39,30 +40,32 @@ class PairwiseTrainer(Trainer):
         Returns:
             Union[torch.Tensor, Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]]: The computed loss and optionally the model outputs.
         """
-        outputs = model(
-            **inputs, output_hidden_states=True, return_dict=True, use_cache=False
-        )
+        outputs = model(**inputs,
+                        output_hidden_states=True,
+                        return_dict=True,
+                        use_cache=False)
         values = outputs.logits
 
-        batch_size = inputs["input_ids"].size(0) // 2
-        chosen_masks, rejected_masks = torch.split(
-            inputs["attention_mask"], batch_size, dim=0
-        )
-        chosen_rewards, rejected_rewards = torch.split(values, batch_size, dim=0)
+        batch_size = inputs['input_ids'].size(0) // 2
+        chosen_masks, rejected_masks = torch.split(inputs['attention_mask'],
+                                                   batch_size,
+                                                   dim=0)
+        chosen_rewards, rejected_rewards = torch.split(values,
+                                                       batch_size,
+                                                       dim=0)
 
         chosen_scores = chosen_rewards.gather(
-            dim=-1, index=(chosen_masks.sum(dim=-1, keepdim=True) - 1)
-        )
+            dim=-1, index=(chosen_masks.sum(dim=-1, keepdim=True) - 1))
         rejected_scores = rejected_rewards.gather(
-            dim=-1, index=(rejected_masks.sum(dim=-1, keepdim=True) - 1)
-        )
+            dim=-1, index=(rejected_masks.sum(dim=-1, keepdim=True) - 1))
 
         chosen_scores, rejected_scores = (
             chosen_scores.squeeze(),
             rejected_scores.squeeze(),
         )
 
-        loss = -F.logsigmoid(chosen_scores.float() - rejected_scores.float()).mean()
+        loss = -F.logsigmoid(chosen_scores.float() -
+                             rejected_scores.float()).mean()
         if return_outputs:
             return loss, (chosen_scores, rejected_scores)
         else:
@@ -80,22 +83,18 @@ class PairwiseTrainer(Trainer):
         if not self.is_world_process_zero():
             return
 
-        output_prediction_file = os.path.join(
-            self.args.output_dir, "generated_predictions.jsonl"
-        )
-        logger.info(f"Saving prediction results to {output_prediction_file}")
+        output_prediction_file = os.path.join(self.args.output_dir,
+                                              'generated_predictions.jsonl')
+        logger.info(f'Saving prediction results to {output_prediction_file}')
         chosen_scores, rejected_scores = predict_results.predictions
 
-        with open(output_prediction_file, "w", encoding="utf-8") as writer:
+        with open(output_prediction_file, 'w', encoding='utf-8') as writer:
             res: List[str] = []
             for c_score, r_score in zip(chosen_scores, rejected_scores):
                 res.append(
-                    json.dumps(
-                        {
-                            "chosen": round(float(c_score), 2),
-                            "rejected": round(float(r_score), 2),
-                        }
-                    )
-                )
+                    json.dumps({
+                        'chosen': round(float(c_score), 2),
+                        'rejected': round(float(r_score), 2),
+                    }))
 
-            writer.write("\n".join(res))
+            writer.write('\n'.join(res))
